9c2fc72abcf1d31da8896a6b394107852670c8c9
idiazjs <idiazlarajesus@gmail.com>
2023-10-13 20:15:50 -0600

    Se agrega rama ejercicio 24 con su respectivo ejercicio

---
+++ code/24_CharArt_ASCII/README.md
@@ -0,0 +1,3 @@
+24) Recorre el String del ejercicio 22 y transforma cada carácter a su código ASCII. Muestralos en linea recta, separados por un espacio entre cada carácter.
+
+La lluvia en Sevilla es una maravilla
---
+++ code/24_CharArt_ASCII/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String texto = "97La lluvia en Sevilla es una maravilla";
+        String ascciTexto = "";
+        for (int i = 0; i < texto.length(); i++) {
+            int x = (int)texto.charAt(i);
+            ascciTexto += x;
+            if(i != texto.length()-1){
+                ascciTexto += ",";
+            }
+        }
+        System.out.println(ascciTexto);
+    }
+}
