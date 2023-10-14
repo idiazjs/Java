ec6618c3cba8453578586746cc97ba60bd4bcc85
idiazjs <idiazlarajesus@gmail.com>
2023-10-13 18:31:39 -0600

    Se actualiza numeros primos

--- code/20_Numeros_Primos/src/App.java
+++ code/20_Numeros_Primos/src/App.java
@@ -4,21 +4,22 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
+        boolean esPrimo = true;
         if (num <= 1) {
-            System.out.println(num + " No es primo");
+            esPrimo = false;
         } else {
-            int raiz = (int)Math.sqrt(num);
-            int cont = 0;
+            int raiz = (int) Math.sqrt(num);
             for (int i = raiz; i > 1; i--) {
                 if (num % i == 0) {
-                    cont += 1;
+                    esPrimo = false;
+                    break;
                 }
             }
-            if (cont < 1){
-                System.out.println(num + " Es primo");
-            }else{
-                System.out.println(num + " No es primo");
-            }
+        }
+        if (esPrimo) {
+            System.out.println(num + " Es primo");
+        } else {
+            System.out.println(num + " No es primo");
         }
         scanner.close();
     }
