5f5d18d80e7a21e42bcf39a5fa089eb63270dd62
idiaz <idiazlarajesus@gmail.com>
2023-10-15 13:17:46 -0600

    Se actualiza Calculadora version final

--- code/25_Calculadora_Polaca_Inversa/src/App.java
+++ code/25_Calculadora_Polaca_Inversa/src/App.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Calculator");
+        Scanner scan = new Scanner(System.in);
+
+        double num1 = scan.nextInt();
+        String operador = scan.next();
+        double num2 = scan.nextInt();
+
+        switch (operador) {
+            case "+":
+                System.out.println(num1 + num2);
+                break;
+            case "-":
+                System.out.println(num1 - num2);
+                break;
+            case "*":
+                System.out.println(num1 * num2);
+                break;
+            case "/":
+                System.out.println(num1 / num2);
+                break;
+            case "^":
+                System.out.println(Math.pow(num1, num2));
+                break;
+            case "%":
+                System.out.println(num1 % num2);
+                break;
+            default:
+                System.out.println("Operador no valido");
+        }
+        scan.close();
     }
 }
