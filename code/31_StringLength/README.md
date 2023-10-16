b39488917ac9886cb5c5c82ff3c038d263d07275
idiaz <idiazlarajesus@gmail.com>
2023-10-16 07:59:53 -0600

    Se agrega StringLength

---
+++ code/31_StringLength/README.md
@@ -0,0 +1,1 @@
+31) Mostrar la longitud de una cadena.
---
+++ code/31_StringLength/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String cadena = "Hola Mundo!";
+        System.out.println(cadena.length());
+    }
+}
