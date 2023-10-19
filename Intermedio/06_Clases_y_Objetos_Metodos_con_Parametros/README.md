6d373924fb98e1e2ac79dac497d28df5ba35bee2
idiaz <idiazlarajesus@gmail.com>
2023-10-19 14:36:06 -0600

    Se agrega Metodos con Parametros

---
+++ Intermedio/06_Clases_y_Objetos_Metodos_con_Parametros/README.md
@@ -0,0 +1,21 @@
+Objetivo de aprendizaje
+Al finalizar este ejercicio, el lector tendrá la capacidad para definir métodos
+con parámetros de entrada.
+
+Enunciado: clase CuentaBancaria
+Se requiere un programa que modele una cuenta bancaria que posee los
+siguientes atributos:
+    > Nombres del titular.
+    > Apellidos del titular.
+    > Número de la cuenta bancaria.
+    > Tipo de cuenta: puede ser una cuenta de ahorros o una cuenta corriente.
+    > Saldo de la cuenta.
+
+Se debe definir un constructor que inicialice los atributos de la clase.
+Cuando se crea una cuenta bancaria, su saldo inicial tiene un valor de cero.
+
+En una determinada cuenta bancaria se puede:
+    > Imprimir por pantalla los valores de los atributos de una cuenta bancaria.
+    > Consultar el saldo de una cuenta bancaria.
+    > Consignar un determinado valor en la cuenta bancaria, actualizando el saldo correspondiente.
+    > Retirar un determinado valor de la cuenta bancaria, actualizando el saldo correspondiente. Es necesario tener en cuenta que no se puede realizar el retiro si el valor solicitado supera el saldo actual de la cuenta.
---
+++ Intermedio/06_Clases_y_Objetos_Metodos_con_Parametros/src/App.java
@@ -0,0 +1,114 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        CuentaBancaria cuentaBancaria = new CuentaBancaria("Jesus", "Diaz", "Lara", "1314",
+                CuentaBancaria.tipoCuenta.AHORROS, 0);
+        System.out.println(cuentaBancaria.getSaldo());
+        cuentaBancaria.depositar(1000);
+        cuentaBancaria.retirar(300);
+        System.out.println(cuentaBancaria.getSaldo());
+        cuentaBancaria.retirar(66);
+        cuentaBancaria.retirar(1000);
+        cuentaBancaria.imprimeCuenta();
+    }
+}
+
+class CuentaBancaria {
+    private String nomTitular;
+    private String appTitular;
+    private String apmTitular;
+    private String numCuenta;
+
+    enum tipoCuenta {
+        AHORROS, CORRIENTE
+    }
+
+    tipoCuenta cuenta;
+    float saldo = 0;
+
+    public CuentaBancaria(String nomTitular, String appTitular, String apmTitular, String numCuenta, tipoCuenta cuenta,
+            float saldo) {
+        this.nomTitular = nomTitular;
+        this.appTitular = appTitular;
+        this.apmTitular = apmTitular;
+        this.numCuenta = numCuenta;
+        this.cuenta = cuenta;
+        this.saldo = saldo;
+    }
+
+    public String getNomTitular() {
+        return this.nomTitular;
+    }
+
+    public void setNomTitular(String nomTitular) {
+        this.nomTitular = nomTitular;
+    }
+
+    public String getAppTitular() {
+        return this.appTitular;
+    }
+
+    public void setAppTitular(String appTitular) {
+        this.appTitular = appTitular;
+    }
+
+    public String getApmTitular() {
+        return this.apmTitular;
+    }
+
+    public void setApmTitular(String apmTitular) {
+        this.apmTitular = apmTitular;
+    }
+
+    public String getNumCuenta() {
+        return this.numCuenta;
+    }
+
+    public void setNumCuenta(String numCuenta) {
+        this.numCuenta = numCuenta;
+    }
+
+    public tipoCuenta getCuenta() {
+        return this.cuenta;
+    }
+
+    public void setCuenta(tipoCuenta cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public float getSaldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public void imprimeCuenta() {
+        System.out.println(nomTitular);
+        System.out.println(appTitular);
+        System.out.println(apmTitular);
+        System.out.println(numCuenta);
+        System.out.println(cuenta);
+        System.out.println(saldo);
+    }
+
+    public void retirar(int valor) {
+        if (valor % 100 != 0) {
+            System.out.println("retirar.Error: Solos multiplos de 100");
+        } else if (valor > saldo) {
+            System.out.println("retirar.Error: Saldo insuficiente");
+        } else {
+            saldo -= valor;
+            System.out.println("retirar.Exito: " + valor);
+        }
+    }
+
+    public void depositar(int valor) {
+        if (valor % 100 != 0) {
+            System.out.println("depositar.Error: Solos multiplos de 100");
+        } else {
+            saldo += valor;
+            System.out.println("depositar.Exito: " + valor);
+        }
+    }
+}
