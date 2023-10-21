da429e515c794d32959a654fc28cdfcecf45347d
idiaz <idiazlarajesus@gmail.com>
2023-10-21 07:25:09 -0600

    Se agrega Sobrecarga de Metodos

---
+++ Intermedio/11_Clases_y_Objetos_Sobrecarga_de_Metodos/README.md
@@ -0,0 +1,21 @@
+Objetivo de aprendizaje
+Al finalizar este ejercicio, el lector tendrá la capacidad para definir métodos
+sobrecargados en una clase.
+
+Enunciado: clase Pedido
+Realizar un programa en Java que permita calcular el pedido que realiza un
+cliente en un restaurante. 
+
+Los pedidos de un restaurante están conformados por las siguientes partes:
+    > Un primer plato.
+    > Un segundo plato.
+    > Una bebida.
+    > Un postre.
+
+Cada uno de dichas partes tiene un nombre y un valor. Se requiere
+definir métodos sobrecargados para calcular el valor del pedido dependiendo si el cliente solicita:
+    > Un primer plato y una bebida.
+    > Un primer plato, un segundo plato y una bebida.
+    > Un primer plato, un segundo plato, una bebida y un postre.
+
+Implementar un método main que utilice los tres métodos sobrecargados en tres diferentes pedidos.
---
+++ Intermedio/11_Clases_y_Objetos_Sobrecarga_de_Metodos/src/App.java
@@ -0,0 +1,42 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Pedido pedido = new Pedido();
+        pedido.calcularPedido("Costillitas", 120.50f, "Coca Cola", 18.50f);
+        pedido.calcularPedido("Costillitas", 120.50f, "Chilaquiles", 80.0f, "Coca Cola", 18.50f);
+        pedido.calcularPedido("Costillitas", 120.50f, "Chilaquiles", 80.0f, "Coca Cola", 18.50f, "Fresas con crema",
+                50f);
+    }
+}
+
+class Pedido {
+
+    public void calcularPedido(String primerPlato, float costoPrimerPlato,
+            String bebida, float costoBebida) {
+        System.out.println("\nFactura...\n");
+        System.out.println(primerPlato + ": $" + costoPrimerPlato);
+        System.out.println(bebida + ": $" + costoBebida);
+        float costo = costoPrimerPlato + costoBebida;
+        System.out.println("Total: " + costo);
+    }
+
+    public void calcularPedido(String primerPlato, float costoPrimerPlato, String segundoPlato, float costoSegundoPlato,
+            String bebida, float costoBebida) {
+        System.out.println("\nFactura...\n");
+        System.out.println(primerPlato + ": $" + costoPrimerPlato);
+        System.out.println(segundoPlato + ": $" + costoSegundoPlato);
+        System.out.println(bebida + ": $" + costoBebida);
+        float costo = costoPrimerPlato + costoSegundoPlato + costoBebida;
+        System.out.println("Total: " + costo);
+    }
+
+    public void calcularPedido(String primerPlato, float costoPrimerPlato, String segundoPlato, float costoSegundoPlato,
+            String bebida, float costoBebida, String postre, float costoPostre) {
+        System.out.println("\nFactura...\n");
+        System.out.println(primerPlato + ": $" + costoPrimerPlato);
+        System.out.println(segundoPlato + ": $" + costoSegundoPlato);
+        System.out.println(bebida + ": $" + costoBebida);
+        System.out.println(postre + ": $" + costoPostre);
+        float costo = costoPrimerPlato + costoSegundoPlato + costoBebida + costoPostre;
+        System.out.println("Total: " + costo);
+    }
+}
