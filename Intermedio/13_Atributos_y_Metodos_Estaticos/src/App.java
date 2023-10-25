94ed9942cd1bd140c6cea4ca9e869a1cdc1abb7f
idiaz <idiazlarajesus@gmail.com>
2023-10-24 20:44:50 -0600

    Se modifica Atributos y Metodos Estaticos

--- Intermedio/13_Atributos_y_Metodos_Estaticos/src/App.java
+++ Intermedio/13_Atributos_y_Metodos_Estaticos/src/App.java
@@ -1,9 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Atleta atleta1 = new Atleta(0, "Alejandro Perlaza", 9.55);
-        Atleta atleta2 = new Atleta(1, "Anthony Zambrano", 9.28);
-        Atleta atleta3 = new Atleta(2, "Diego Palomeque", 9.53);
-        Atleta atleta4 = new Atleta(3, "Gilmar Herrera", 9.29);
+        Atleta atleta1 = new Atleta("Alejandro Perlaza", 9.55);
+        Atleta atleta2 = new Atleta("Anthony Zambrano", 9.28);
+        Atleta atleta3 = new Atleta("Diego Palomeque", 9.53);
+        Atleta atleta4 = new Atleta("Gilmar Herrera", 9.29);
         Atleta.imprimeSeleccion();
         atleta1.correr400m();
         Atleta.imprimeTiempoEquipo();
@@ -13,30 +13,28 @@ public class App {
         atleta4.correr400m();
         Atleta.imprimeTiempoEquipo();
         Atleta.imprimeTotalAtletas();
+
+        System.out.println();
+        atleta1.imprimeAtleta();
+
+        System.out.println();
+        atleta2.imprimeAtleta();
     }
 }
 
 class Atleta {
-    private int idAtleta;
+    private int idAtleta = 0;
     private String nomAtleta;
     private double tiempoAtleta;
     private static int numAtletas = 0;
     private static String seleccion = "Colombia";
     private static double tiempoEquipo;
 
-    public Atleta(int idAtleta, String nomAtleta, double tiempoAtleta) {
-        this.idAtleta = idAtleta;
+    public Atleta(String nomAtleta, double tiempoAtleta) {
+        this.idAtleta = numAtletas + 10;
         this.nomAtleta = nomAtleta;
         this.tiempoAtleta = tiempoAtleta;
         numAtletas += 1;
-    }
-
-    public int getIdAtleta() {
-        return this.idAtleta;
-    }
-
-    public void setIdAtleta(int idAtleta) {
-        this.idAtleta = idAtleta;
     }
 
     public String getNomAtleta() {
@@ -64,11 +62,17 @@ class Atleta {
     }
 
     public static void imprimeTiempoEquipo() {
-        System.out.println(tiempoEquipo);
+        System.out.println("Tiempo del equipo: " + tiempoEquipo);
     }
 
     public static void imprimeTotalAtletas() {
-        System.out.println(numAtletas);
+        System.out.println("Total de atletas: " + numAtletas);
+    }
+
+    public void imprimeAtleta() {
+        System.out.println("id: " + idAtleta);
+        System.out.println("Nombre: " + nomAtleta);
+        System.out.println("Tiempo: " + tiempoAtleta);
     }
 
 }
