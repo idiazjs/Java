e4e8c65488b4ba5c93527287affb403e063a3bd7
idiaz <idiazlarajesus@gmail.com>
2023-11-04 09:33:12 -0600

    Se agregan metodos a Clase String

--- Intermedio/14_Clase_String/README.md
+++ Intermedio/14_Clase_String/README.md
@@ -10,3 +10,10 @@ Se desea construir un programa que dado un String con valor inicial de:
     5. Extraer del String (obtenido en el paso anterior), un Substring desde la posición 10 al 15.
     6. Reemplazar en el String (obtenido en el paso anterior) el carácter “O” por “o”.
     7. Comparar el String (obtenido en el paso anterior) con el String “Programacion”. Después de cada paso, se debe mostrar el resultado en pantalla.
+
+Ejercicios propuestos
+Hacer una clase que contenga métodos que realicen las siguientes acciones:
+    > Un método que reciba como parámetro un String y calcule cuántas mayúsculas tiene.
+    > Un método que reciba como parámetro un String y una letra, y determine cuántas veces está la letra en el String   (la letra puede estar en mayúscula o minúscula).
+    > Un método que reciba como parámetro un String, y que elimine todos sus espacios en blanco y escriba en pantalla el    String resultante.
+    > Un método que reciba como parámetro un String, y obtenga el String escrito al revés.
--- Intermedio/14_Clase_String/src/App.java
+++ Intermedio/14_Clase_String/src/App.java
@@ -13,5 +13,54 @@ public class App {
         text = text.replace("O", "o");
         System.out.println(text);
         System.out.println(text.equals("Programacion"));
+        cuentaMayus(text);
+        calculaApariciones(text, 'O');
+        eliminaEspacios(text);
+        invierteString(text);
     }
+
+    // Cuenta el total de mayusculas
+    public static void cuentaMayus(String x) {
+        char c;
+        int cont = 0;
+        for (int i = 0; i < x.length(); i++) {
+            c = x.charAt(i);
+            if (Character.isUpperCase(c)) {
+                cont += 1;
+            }
+        }
+        System.out.println(cont);
+    }
+
+    public static void calculaApariciones(String text, char c) {
+        text = text.toLowerCase();
+        c = Character.toLowerCase(c);
+        int cont = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == c) {
+                cont += 1;
+            }
+        }
+        System.out.println(cont);
+    }
+
+    public static void eliminaEspacios(String text) {
+        char c = ' ';
+        String nuevoString = "";
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) != c) {
+                nuevoString += String.valueOf(text.charAt(i));
+            }
+        }
+        System.out.println(nuevoString);
+    }
+
+    public static void invierteString(String text) {
+        String nuevoString = "";
+        for (int i = text.length() - 1; i >= 0; i--) {
+            nuevoString += String.valueOf(text.charAt(i));
+        }
+        System.out.println(nuevoString);
+    }
+
 }
